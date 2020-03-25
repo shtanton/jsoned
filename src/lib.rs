b3d2f5d4351f2b428f533b7252961da49efcf559
shtanton <charlie@shtanton.com>
2020-03-25 16:59:29 +0000

    Starts switching to a plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use std::iter::once;
 
-use skulpin::app::AppDrawArgs;
-use skulpin::skia_safe::{Point, Rect};
-
 pub enum JsonBufferMode {
 	Normal,
 	Insert,
@@ -24,19 +21,129 @@ pub enum JsonInput {
 }
 
 pub struct JsonNode {
-	pub variant: JsonVariant,
-	pub parent: usize,
-	pub left: usize,
-	pub right: usize,
+	variant: JsonVariant,
+	parent: usize,
+	left: usize,
+	right: usize,
+	lines: usize,
+	indent: usize,
 }
 
 pub struct JsonBuffer {
-	pub nodes: Vec<JsonNode>,
-	pub selections: Vec<usize>,
-	pub mode: JsonBufferMode,
+	nodes: Vec<JsonNode>,
+	selections: Vec<usize>,
+	mode: JsonBufferMode,
 }
 
 impl JsonBuffer {
+	fn position(&self, node_index: usize) -> (usize, usize, usize, usize) {
+		let node = self.nodes[node_index];
+		let parent_index = node.parent;
+		if node_index == parent_index {
+			(0, 0, 0, node.lines)
+		} else {
+			let (parent_left, parent_top, parent_right, parent_bottom) = self.position(parent_index);
+			let mut last_line_width = match node.variant {
+				JsonVariant::Null => 4,
+				JsonVariant::Bool(b) => if b {4} else {5},
+				JsonVariant::Number(n) => n.to_string().len(),
+				JsonVariant::String(s) => s.len(),
+				JsonVariant::Object(_) => 1,
+				JsonVariant::Array(_) => 1,
+			};
+			let parent = self.nodes[parent_index];
+			match parent.variant {
+				JsonVariant::ObjectEntry(key, _) => (parent_left + key.len() + 4, parent_top, last_line_width + 1, parent_top + node.lines),
+				JsonVariant::Array(ref children) => {
+				},
+			}
+		}
+	}
+	fn insert_null_index(&mut self, parent_index: usize, index: usize) {
+		match self.nodes[parent_index].variant {
+			JsonVariant::Array(ref mut children) => {
+				let mut child = JsonNode {
+					variant: JsonVariant::Null,
+					parent: parent_index,
+					left: 0,
+					right: 0,
+					lines: 0,
+					indent: 0,
+				};
+				let child_index = self.nodes.len();
+				child.indent = self.nodes[parent_index].indent + 1;
+				children.insert(index, child_index);
+				child.left = parent_index;
+				if index != 0 {
+					child.left = children[index-1];
+					self.nodes[child.left].right = child_index;
+				}
+				child.right = parent_index;
+				if index != children.len() {
+					child.right = children[index+1];
+					self.nodes[child.right].left = child_index;
+				}
+				self.nodes.push(child);
+			},
+			JsonVariant::Object(ref mut children) => {
+				let entry_index = self.nodes.len();
+				let value_index = entry_index + 1;
+				let indent = self.nodes[parent_index].indent + 1;
+				children.insert(index, entry_index);
+				let mut left = parent_index;
+				if index != 0 {
+					left = children[index-1];
+					self.nodes[left].right = entry_index;
+				}
+				let mut right = parent_index;
+				if index != children.len() {
+					right = children[index+1];
+					self.nodes[right].left = entry_index;
+				}
+				self.nodes.push(JsonNode {
+					variant: JsonVariant::ObjectEntry("".to_string(), value_index),
+					parent: parent_index,
+					left: left,
+					right: right,
+					lines: 0,
+					indent: indent,
+				});
+				self.nodes.push(JsonNode {
+					variant: JsonVariant::Null,
+					parent: entry_index,
+					left: entry_index,
+					right: entry_index,
+					lines: 0,
+					indent: indent,
+				});
+			},
+			_ => panic!("Tried to insert into a node without children"),
+		}
+	}
+	fn insert_null_left(&mut self, sibling: usize) {
+		let index = match self.nodes[self.nodes[sibling].parent].variant {
+			JsonVariant::Array(ref mut children) => {
+				children.iter().position(|mi| *mi==sibling).unwrap()
+			},
+			JsonVariant::Object(ref mut children) => {
+				children.iter().position(|mi| *mi==sibling).unwrap()
+			},
+			_ => panic!("Tried to insert into a node without children"),
+		};
+		self.insert_null_index(self.nodes[sibling].parent, index);
+	}
+	fn insert_null_right(&mut self, sibling: usize) {
+		let index = match self.nodes[self.nodes[sibling].parent].variant {
+			JsonVariant::Array(ref mut children) => {
+				children.iter().position(|mi| *mi==sibling).unwrap()+1
+			},
+			JsonVariant::Object(ref mut children) => {
+				children.iter().position(|mi| *mi==sibling).unwrap()+1
+			},
+			_ => panic!("Tried to insert into a node without children"),
+		};
+		self.insert_null_index(self.nodes[sibling].parent, index);
+	}
 	pub fn select_up(&mut self) {
 		let new_selections = self.selections.iter().map(|index| {
 			self.nodes[*index].left
@@ -94,68 +201,6 @@ impl JsonBuffer {
 			iter
 		});
 		self.selections = new_selections.collect();
-	}
-	pub fn new_first_child(&mut self) {
-		let mut new_selections = Vec::with_capacity(self.selections.len());
-		for selection_index in self.selections.iter() {
-			let new_index = self.nodes.len();
-			match self.nodes[*selection_index].variant {
-				JsonVariant::Array(ref mut children) => {
-					if children.len() == 0 {
-						children.push(new_index);
-						self.nodes.push(JsonNode {
-							variant: JsonVariant::Null,
-							parent: *selection_index,
-							left: *selection_index,
-							right: *selection_index,
-						});
-					} else {
-						let old_first = children[0];
-						children.insert(0, new_index);
-						self.nodes.push(JsonNode {
-							variant: JsonVariant::Null,
-							parent: *selection_index,
-							left: *selection_index,
-							right: old_first,
-						});
-						self.nodes[old_first].left = new_index;
-					}
-					new_selections.push(new_index);
-				},
-				JsonVariant::Object(ref mut children) => {
-					if children.len() == 0 {
-						children.push(new_index);
-						self.nodes.push(JsonNode {
-							variant: JsonVariant::ObjectEntry("".to_string(), new_index+1),
-							parent: *selection_index,
-							left: *selection_index,
-							right: *selection_index,
-						});
-					} else {
-						let old_first = children[0];
-						children.insert(0, new_index);
-						self.nodes.push(JsonNode {
-							variant: JsonVariant::ObjectEntry("".to_string(), new_index+1),
-							parent: *selection_index,
-							left: *selection_index,
-							right: old_first,
-						});
-						self.nodes[old_first].left = new_index;
-					}
-					self.nodes.push(JsonNode {
-						variant: JsonVariant::Null,
-						parent: new_index,
-						left: new_index,
-						right: new_index,
-					});
-					new_selections.push(new_index);
-				},
-				_ => {
-					new_selections.push(*selection_index);
-				},
-			}
-		};
-		self.selections = new_selections;
 	}
 	pub fn new_up_sibling(&mut self) {
 		let mut new_selections: Vec<usize> = Vec::with_capacity(self.selections.len());
